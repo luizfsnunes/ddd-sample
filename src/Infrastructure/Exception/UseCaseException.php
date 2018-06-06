4c13c2c7205c7a3ab84007d927851e902af9a300
luizfsnunes <luizfsnunes@gmail.com>
2018-06-06 08:21:42 -0300

    Adicionar exceção específica para Use Case

---
+++ src/Infrastructure/Exception/UseCaseException.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Infrastructure\Exception;
+
+use Symfony\Component\Config\Definition\Exception\Exception;
+
+class UseCaseException extends Exception
+{
+
+}
